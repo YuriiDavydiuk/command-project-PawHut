efecc45815023c73fc6e29036904a1b03f444ea2
Yurii <davydiuk.yurii@gmail.com>
2026-03-09 08:38:29 +0200

    fix: testimonials.html

--- src/main.js
+++ src/main.js
@@ -12,10 +12,8 @@ import './js/burger.js';
 import './js/slider.js';
 import './js/gallery-slider.js';
 import './js/accordion.js';
-import './js/filter.js';
-// import './js/pagination.js';
 import './js/product.js';
-// import './js/adopt-modal.js';
+
 
 // Модалки
 import './js/modal.js';
