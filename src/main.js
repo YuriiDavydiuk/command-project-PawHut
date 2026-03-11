2fc27c4cfd44b1b4c0004954e9e574272f8246fd
Yurii <davydiuk.yurii@gmail.com>
2026-03-11 09:56:05 +0200

    fix: js logic

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 // DOM refs
 import './js/refs.js';
 
-// API
-import { fetchCategories, fetchAnimals } from './js/api.js';
-
-// Render
-import { renderAnimalCards, renderCategories } from './js/render.js';
-
 // UI логіка
 import './js/burger.js';
 import './js/slider.js';
 import './js/gallery-slider.js';
 import './js/accordion.js';
 import './js/product.js';
-
 
 // Модалки
 import './js/modal.js';
